3ea3875e6f94d0ee094b30138bdbb653782a8bba
serap-atay <75393492+serap-atay@users.noreply.github.com>
2022-03-11 16:10:32 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import About from "./routingSample/About";
+import Contact from "./routingSample/Contact";
+import Home from "./routingSample/Home";
+import SupplierDetail from "./routingSample/SupplierDetail";
+import SupplierPage from "./routingSample/SupplierPage";
+
+
 function App() {
 
   const group = {
@@ -8,12 +15,23 @@ function App() {
       albumCount: 5
     }
   }
+
+
+
+
   return (
-    <></>
-  )
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/suppliers' element={<SupplierPage/>}></Route>
+        <Route path='/suppliers/detail/:id' element={<SupplierDetail/>}></Route>
+      </Routes>
+
+    </>
+
+  );
 }
 
 export default App;
-//class component mi funtion component le diye ikiye ayrılıyor.
-//jsx buranın cshtmlidir.
-//javascripte scope ile geçiyoruz ordan JSX e parantez ile geçiyoruz.
